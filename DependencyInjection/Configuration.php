0f27ebdc6465bc227a80e13de7a3372f699f5f80
Pedro Carlos Abreu <pcabreus@gmail.com>
2017-03-12 19:07:53 -0400

    Chnage the extension to positibe_cmf_routing_extra

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('positibe_routing');
+        $rootNode = $treeBuilder->root('positibe_cmf_routing_extra');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
